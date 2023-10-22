b2088b395d8616fe64e4e68974de7c4f577735a7
guyettinger <guy@pointivo.com>
2023-10-22 00:56:37 -0400

    storybook story format

--- src/components/SceneView/SceneView.stories.tsx
+++ src/components/SceneView/SceneView.stories.tsx
@@ -14,38 +14,38 @@ export default meta;
 
 type Story = StoryObj<typeof SceneView>;
 
-const farmGeodeticCenter = new Vector3(-83.765350, 34.401279, 357.0)
-const farmCameraGeodeticCenter = new Vector3(farmGeodeticCenter.x, farmGeodeticCenter.y, farmGeodeticCenter.z + 10)
+export const Farm: Story = (args: any) => {
+    const farmGeodeticCenter = new Vector3(-83.765350, 34.401279, 357.0)
+    const farmCameraGeodeticCenter = new Vector3(farmGeodeticCenter.x, farmGeodeticCenter.y, farmGeodeticCenter.z + 10)
 
-const sceneModel: SceneModel = new SceneModel(
-    'Scene1',
-    <group>
-        <Box position={[4, 0, 0]}/>
-        <Box position={[0, 0, -4]}/>
-        <Box position={[-4, 0, 0]}/>
-        <PointCloud
-            baseUrl="https://raw.githubusercontent.com/potree/potree/develop/pointclouds/lion_takanawa/"
-            fileName="cloud.js"
-            position={[0, -.75, 0]}
-        />
-        <PointCloud
-            baseUrl="https://raw.githubusercontent.com/potree/potree/develop/pointclouds/lion_takanawa/"
-            fileName="cloud.js"
-            position={[10, -.75, 0]}
-        />
-    </group>,
-    farmGeodeticCenter
-)
+    const sceneModel: SceneModel = new SceneModel(
+        'Scene1',
+        <group>
+            <Box position={[4, 0, 0]}/>
+            <Box position={[0, 0, -4]}/>
+            <Box position={[-4, 0, 0]}/>
+            <PointCloud
+                baseUrl="https://raw.githubusercontent.com/potree/potree/develop/pointclouds/lion_takanawa/"
+                fileName="cloud.js"
+                position={[0, -.75, 0]}
+            />
+            <PointCloud
+                baseUrl="https://raw.githubusercontent.com/potree/potree/develop/pointclouds/lion_takanawa/"
+                fileName="cloud.js"
+                position={[10, -.75, 0]}
+            />
+        </group>,
+        farmGeodeticCenter
+    )
 
-const sceneViewModel: SceneViewModel = new SceneViewModel(
-    'SceneView1',
-    sceneModel,
-    farmCameraGeodeticCenter
-)
+    const sceneViewModel: SceneViewModel = new SceneViewModel(
+        'SceneView1',
+        sceneModel,
+        farmCameraGeodeticCenter
+    )
 
-export const Farm: Story = (args: any) => (
-    <SceneView data-testId="SceneView-id" {...args} />
-);
-Farm.args = {
-    sceneViewModel: sceneViewModel
-};
+    return (
+    <SceneView data-testId="SceneView-id" sceneViewModel={sceneViewModel} />
+    )
+}
+Farm.args = {};
--- src/components/SceneViewInformation/SceneViewInformation.stories.tsx
+++ src/components/SceneViewInformation/SceneViewInformation.stories.tsx
@@ -13,24 +13,25 @@ export default meta;
 
 type Story = StoryObj<typeof SceneViewInformation>;
 
-const farmGeodeticCenter = new Vector3(-83.764977, 34.401379, 400.0)
-const farmCameraGeodeticCenter = new Vector3(farmGeodeticCenter.x, farmGeodeticCenter.y, farmGeodeticCenter.z + 100)
+export const Farm: Story = (args: any) => {
+    const farmGeodeticCenter = new Vector3(-83.764977, 34.401379, 400.0)
+    const farmCameraGeodeticCenter = new Vector3(farmGeodeticCenter.x, farmGeodeticCenter.y, farmGeodeticCenter.z + 100)
 
-const sceneModel: SceneModel = new SceneModel(
-    'Scene1',
-    <Box position={[0, 0, 0]}/>,
-    farmGeodeticCenter
-)
+    const sceneModel: SceneModel = new SceneModel(
+        'Scene1',
+        <Box position={[0, 0, 0]}/>,
+        farmGeodeticCenter
+    )
 
-const sceneViewModel: SceneViewModel = new SceneViewModel(
-    'SceneView1',
-    sceneModel,
-    farmCameraGeodeticCenter
-)
+    const sceneViewModel: SceneViewModel = new SceneViewModel(
+        'SceneView1',
+        sceneModel,
+        farmCameraGeodeticCenter
+    )
 
-export const Farm: Story = (args: any) => (
-    <SceneViewInformation data-testId="SceneView-id" {...args} />
-);
-Farm.args = {
-    sceneViewModel: sceneViewModel
-};
+    return (
+        <SceneViewInformation data-testId="SceneView-id" sceneViewModel={sceneViewModel}/>
+    )
+}
+
+Farm.args = {};
