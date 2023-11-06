796113b3dcfd68c1847447ed8a018eb677505062
guyettinger <guy@pointivo.com>
2023-11-06 12:03:17 -0500

    documentation

--- README.md
+++ README.md
@@ -1,8 +1,96 @@
+<img alt="logo" src="public/images/logo.png" style="float: left;" width="128"/>
+
 # gle-scene-components
-A component library for building 3D scenes.
+A React component library for building 3D scenes.
 
 ## Documentation
 [Storybook](https://guyettinger.github.io/gle-scene-components/)
+
+## Example
+![example-screenshot.png](public%2Fimages%2Fexample-screenshot.png)
+```tsx
+// coordinates
+const upperArenaLongitudeLatitudeHeight = new Vector3(-83.765350, 34.401279, 357.0)
+const barnParkingLotLongitudeLatitudeHeight = new Vector3(-83.76536188233062, 34.400715493095205, 353.0)
+const lowerArenaLongitudeLatitudeHeight = new Vector3(-83.76612684589652, 34.40024525982904, 350.0)
+
+// create a scene
+const sceneModel: SceneModel = new SceneModel(
+    'Scene1',
+    <group>
+        <CoordinatedGroup longitudeLatitudeHeight={upperArenaLongitudeLatitudeHeight}>
+            <PointCloud
+                baseUrl="https://raw.githubusercontent.com/potree/potree/develop/pointclouds/lion_takanawa/"
+                fileName="cloud.js"
+                onPointCloudLoad={rotatePointCloudOctreeYUp}
+                position={[0, -.75, 0]}
+            />
+        </CoordinatedGroup>
+        <CoordinatedGroup longitudeLatitudeHeight={barnParkingLotLongitudeLatitudeHeight}>
+            <Box position={[0, 0, 0]}/>
+        </CoordinatedGroup>
+        <CoordinatedGroup longitudeLatitudeHeight={lowerArenaLongitudeLatitudeHeight}>
+            <GaussianSplatCloud baseUrl="./"
+                                fileName="splats/ornament/ornament.splat"
+                                position={[0, 1, 0]}
+                                rotation={[
+                                    MathUtils.degToRad(-30),
+                                    MathUtils.degToRad(-45),
+                                    MathUtils.degToRad(180), 'ZYX']}
+            />
+        </CoordinatedGroup>
+    </group>,
+    upperArenaLongitudeLatitudeHeight
+)
+
+// create a view of the scene
+const sceneViewModel: SceneViewModel = new SceneViewModel(
+    'SceneView1',
+    sceneModel
+)
+
+export const ExampleScene = () => {
+    return (
+        <>
+            <button
+                onClick={() => sceneViewModel.moveCameraToLongitudeLatitudeHeight(upperArenaLongitudeLatitudeHeight)}>
+                Upper Arena
+            </button>
+            <button
+                onClick={() => sceneViewModel.moveCameraToLongitudeLatitudeHeight(barnParkingLotLongitudeLatitudeHeight)}>
+                Parking
+            </button>
+            <button
+                onClick={() => sceneViewModel.moveCameraToLongitudeLatitudeHeight(lowerArenaLongitudeLatitudeHeight)}>
+                Lower Arena
+            </button>
+            <SceneView sceneViewModel={sceneViewModel}/>
+        </>
+    )
+}
+```
+
+## Installation
+```shell
+npm install gle-scene-components@latest
+```
+
+## Configuration
+### Configure Resium
+Follow the [Resium Install Guide ](https://resium.reearth.io/installation)
+
+### Configure Cross-origin Isolation
+Gaussian Splatting Shared Memory requires [Cross-origin Isolation to be configured](https://web.dev/articles/coop-coep) in deployment.
+
+This can be achieved by setting response headers on your server:
+```
+response.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+response.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+```
+... or deploying [coi-serviceworker](https://github.com/gzuidhof/coi-serviceworker) in your webpage:
+```html
+<script src="coi-serviceworker.js"></script>
+```
 
 ## Development
 Install
@@ -20,20 +108,4 @@ npm run test
 Run Storybook
 ```
 npm run storybook
-```
-
-## Configure Resium
-Follow the [Resium Install Guide ](https://resium.reearth.io/installation)
-
-## Configure Cross-origin Isolation
-Gaussian Splatting Shared Memory requires [Cross-origin Isolation to be configured](https://web.dev/articles/coop-coep) in deployment.
-
-This can be achieved by setting response headers on your server:
-```
-response.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-response.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-```
-... or deploying [coi-serviceworker](https://github.com/gzuidhof/coi-serviceworker) in your webpage:
-```html
-<script src="coi-serviceworker.js"></script>
 ```
--- src/components/SceneView/SceneView.stories.tsx
+++ src/components/SceneView/SceneView.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { KeyboardEvent } from "react";
-import { Euler, MathUtils, Vector3 } from "three";
+import { MathUtils, Vector3 } from "three";
+import styled from "styled-components";
 import { SceneModel, SceneViewModel } from "../../models";
 import { SceneView } from "./SceneView";
 import { Box } from "../Box";
@@ -8,7 +8,6 @@ import { PointCloud } from "../PointCloud";
 import { GaussianSplatCloud } from "../GaussianSplatCloud";
 import { CoordinatedGroup } from "../CoordinatedGroup";
 import { rotatePointCloudOctreeYUp } from "../../services";
-import styled from "styled-components";
 
 const meta: Meta<typeof SceneView> = {
     component: SceneView,
@@ -18,6 +17,21 @@ const meta: Meta<typeof SceneView> = {
 export default meta;
 
 type Story = StoryObj<typeof SceneView>;
+
+const SceneButton = styled.button`
+  position: relative;
+  margin-right: 10px;
+  padding: 4px 8px;
+  z-index: 10;
+  background: rgba(255, 255, 255, 0.25);
+  border: 1px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.5);
+    border: 1px rgba(0, 0, 0, 0.5);
+  }
+`
 
 export const Boxes: Story = (args: any) => {
 
@@ -89,12 +103,6 @@ export const PointClouds: Story = (args: any) => {
                 onPointCloudLoad={rotatePointCloudOctreeYUp}
                 position={[0, -.75, 0]}
             />
-            <PointCloud
-                baseUrl="https://raw.githubusercontent.com/potree/potree/develop/pointclouds/lion_takanawa/"
-                fileName="cloud.js"
-                onPointCloudLoad={rotatePointCloudOctreeYUp}
-                position={[10, -.75, 0]}
-            />
         </group>,
         upperArenaLongitudeLatitudeHeight
     )
@@ -115,26 +123,18 @@ export const GaussianSplatClouds: Story = (args: any) => {
     // coordinates
     const upperArenaLongitudeLatitudeHeight = new Vector3(-83.765350, 34.401279, 357.0)
 
-    // position of center of the splat cloud
-    const positionX = 0
-    const positionY = 2
-    const positionZ = 0
-    const position = new Vector3(positionX, positionY, positionZ)
-
-    // rotation of the splat cloud
-    const rotateX = MathUtils.degToRad(-30)
-    const rotateY = MathUtils.degToRad(-45)
-    const rotateZ = MathUtils.degToRad(180)
-    const rotation = new Euler(rotateX, rotateY, rotateZ, 'ZYX')
-
     // create a scene with a garden splat cloud in the farm's upper arena
     const sceneModel: SceneModel = new SceneModel(
         'Scene1',
         <group>
             <GaussianSplatCloud baseUrl="./"
                                 fileName="splats/ornament/ornament.splat"
-                                position={position}
-                                rotation={rotation}/>
+                                position={[0, 2, 0]}
+                                rotation={[
+                                    MathUtils.degToRad(-30),
+                                    MathUtils.degToRad(-45),
+                                    MathUtils.degToRad(180), 'ZYX']}
+            />
         </group>,
         upperArenaLongitudeLatitudeHeight
     )
@@ -150,69 +150,13 @@ export const GaussianSplatClouds: Story = (args: any) => {
 }
 GaussianSplatClouds.args = {};
 
-export const Mixed: Story = (args: any) => {
-
-    // coordinates
-    const upperArenaLongitudeLatitudeHeight = new Vector3(-83.765350, 34.401279, 357.0)
-
-    // rotation of the splat cloud
-    const rotateX = MathUtils.degToRad(-30)
-    const rotateY = MathUtils.degToRad(-45)
-    const rotateZ = MathUtils.degToRad(180)
-    const rotation = new Euler(rotateX, rotateY, rotateZ, 'ZYX')
-
-    // create a scene with multiple types of 3D elements in the farm's upper arena
-    const sceneModel: SceneModel = new SceneModel(
-        'Scene1',
-        <group>
-            <Box position={[-4, 0, 0]}/>
-            <PointCloud
-                baseUrl="https://raw.githubusercontent.com/potree/potree/develop/pointclouds/lion_takanawa/"
-                fileName="cloud.js"
-                onPointCloudLoad={rotatePointCloudOctreeYUp}
-                position={[0, -.75, 0]}
-            />
-            <GaussianSplatCloud baseUrl="./"
-                                fileName="splats/ornament/ornament.splat"
-                                position={[4,2,0]}
-                                rotation={rotation}/>
-        </group>,
-        upperArenaLongitudeLatitudeHeight
-    )
-
-    const sceneViewModel: SceneViewModel = new SceneViewModel(
-        'SceneView1',
-        sceneModel
-    )
-
-    return (
-        <SceneView data-testid="SceneView-id" sceneViewModel={sceneViewModel}/>
-    )
-}
-Mixed.args = {};
-
-const SceneButton = styled.button`
-  position: relative;
-  margin-right: 10px;
-  padding: 4px 8px;
-  z-index: 10;
-  background: rgba(255, 255, 255, 0.25);
-  border: 1px rgba(0, 0, 0, 0.25);
-  border-radius: 4px;
-
-  &:hover {
-    background: rgba(255, 255, 255, 0.5);
-    border: 1px rgba(0, 0, 0, 0.5);
-  }
-`
-
 
 export const CoordinatedGroups: Story = (args: any) => {
 
     // coordinates
     const upperArenaLongitudeLatitudeHeight = new Vector3(-83.765350, 34.401279, 357.0)
-    const lowerArenaLongitudeLatitudeHeight = new Vector3(-83.76612684589652, 34.40024525982904, 350.0)
-    const railayBeachLongitudeLatitudeHeight = new Vector3(98.83703938567413, 8.012837715484707, 0)
+    const barnParkingLotLongitudeLatitudeHeight = new Vector3(-83.76536188233062, 34.400715493095205, 353.0)
+    const lowerArenaLongitudeLatitudeHeight = new Vector3(-83.76612684589652, 34.40024525982904, 349.0)
 
     // create a scene with boxes in the upper arena, the lower arena, and at Railay Beach
     const sceneModel: SceneModel = new SceneModel(
@@ -220,24 +164,12 @@ export const CoordinatedGroups: Story = (args: any) => {
         <group>
             <CoordinatedGroup longitudeLatitudeHeight={upperArenaLongitudeLatitudeHeight}>
                 <Box position={[0, 0, 0]}/>
-                <Box position={[4, 0, 0]}/>
-                <Box position={[0, 0, -4]}/>
-                <Box position={[-4, 0, 0]}/>
-                <Box position={[0, 4, 0]}/>
+            </CoordinatedGroup>
+            <CoordinatedGroup longitudeLatitudeHeight={barnParkingLotLongitudeLatitudeHeight}>
+                <Box position={[0, 0, 0]}/>
             </CoordinatedGroup>
             <CoordinatedGroup longitudeLatitudeHeight={lowerArenaLongitudeLatitudeHeight}>
                 <Box position={[0, 0, 0]}/>
-                <Box position={[4, 0, 0]}/>
-                <Box position={[0, 0, -4]}/>
-                <Box position={[-4, 0, 0]}/>
-                <Box position={[0, 4, 0]}/>
-            </CoordinatedGroup>
-            <CoordinatedGroup longitudeLatitudeHeight={railayBeachLongitudeLatitudeHeight}>
-                <Box position={[0, 0, 0]}/>
-                <Box position={[4, 0, 0]}/>
-                <Box position={[0, 0, -4]}/>
-                <Box position={[-4, 0, 0]}/>
-                <Box position={[0, 4, 0]}/>
             </CoordinatedGroup>
         </group>,
         upperArenaLongitudeLatitudeHeight
@@ -250,17 +182,82 @@ export const CoordinatedGroups: Story = (args: any) => {
 
     return (
         <>
-            <SceneButton onClick={()=>sceneViewModel.moveCameraToLongitudeLatitudeHeight(upperArenaLongitudeLatitudeHeight)}>
+            <SceneButton
+                onClick={() => sceneViewModel.moveCameraToLongitudeLatitudeHeight(upperArenaLongitudeLatitudeHeight)}>
                 Upper Arena
             </SceneButton>
-            <SceneButton onClick={()=>sceneViewModel.moveCameraToLongitudeLatitudeHeight(lowerArenaLongitudeLatitudeHeight)}>
-                Lower Arena
+            <SceneButton
+                onClick={() => sceneViewModel.moveCameraToLongitudeLatitudeHeight(barnParkingLotLongitudeLatitudeHeight)}>
+                Parking
             </SceneButton>
-            <SceneButton onClick={()=>sceneViewModel.moveCameraToLongitudeLatitudeHeight(railayBeachLongitudeLatitudeHeight)}>
-                Railay Beach
+            <SceneButton
+                onClick={() => sceneViewModel.moveCameraToLongitudeLatitudeHeight(lowerArenaLongitudeLatitudeHeight)}>
+                Lower Arena
             </SceneButton>
             <SceneView data-testid="SceneView-id" tabIndex={0} sceneViewModel={sceneViewModel}/>
         </>
     )
 }
 CoordinatedGroups.args = {};
+
+
+export const Everything: Story = (args: any) => {
+
+    // coordinates
+    const upperArenaLongitudeLatitudeHeight = new Vector3(-83.765350, 34.401279, 357.0)
+    const barnParkingLotLongitudeLatitudeHeight = new Vector3(-83.76536188233062, 34.400715493095205, 353.0)
+    const lowerArenaLongitudeLatitudeHeight = new Vector3(-83.76612684589652, 34.40024525982904, 350.0)
+
+    // create a scene with multiple types of 3D elements in the farm's upper arena
+    const sceneModel: SceneModel = new SceneModel(
+        'Scene1',
+        <group>
+            <CoordinatedGroup longitudeLatitudeHeight={upperArenaLongitudeLatitudeHeight}>
+                <PointCloud
+                    baseUrl="https://raw.githubusercontent.com/potree/potree/develop/pointclouds/lion_takanawa/"
+                    fileName="cloud.js"
+                    onPointCloudLoad={rotatePointCloudOctreeYUp}
+                    position={[0, -.75, 0]}
+                />
+            </CoordinatedGroup>
+            <CoordinatedGroup longitudeLatitudeHeight={barnParkingLotLongitudeLatitudeHeight}>
+                <Box position={[0, 0, 0]}/>
+            </CoordinatedGroup>
+            <CoordinatedGroup longitudeLatitudeHeight={lowerArenaLongitudeLatitudeHeight}>
+                <GaussianSplatCloud baseUrl="./"
+                                    fileName="splats/ornament/ornament.splat"
+                                    position={[0, 1, 0]}
+                                    rotation={[
+                                        MathUtils.degToRad(-30),
+                                        MathUtils.degToRad(-45),
+                                        MathUtils.degToRad(180), 'ZYX']}
+                />
+            </CoordinatedGroup>
+        </group>,
+        upperArenaLongitudeLatitudeHeight
+    )
+
+    const sceneViewModel: SceneViewModel = new SceneViewModel(
+        'SceneView1',
+        sceneModel
+    )
+
+    return (
+        <>
+            <SceneButton
+                onClick={() => sceneViewModel.moveCameraToLongitudeLatitudeHeight(upperArenaLongitudeLatitudeHeight)}>
+                Upper Arena
+            </SceneButton>
+            <SceneButton
+                onClick={() => sceneViewModel.moveCameraToLongitudeLatitudeHeight(barnParkingLotLongitudeLatitudeHeight)}>
+                Parking
+            </SceneButton>
+            <SceneButton
+                onClick={() => sceneViewModel.moveCameraToLongitudeLatitudeHeight(lowerArenaLongitudeLatitudeHeight)}>
+                Lower Arena
+            </SceneButton>
+            <SceneView data-testid="SceneView-id" sceneViewModel={sceneViewModel}/>
+        </>
+    )
+}
+Everything.args = {};
