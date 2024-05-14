d533ced58923c94e36ea44e6b735e18eb9242338
guyettinger <guy@flaire.co>
2024-05-14 00:22:52 -0400

    Cesium Access Token

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,9 @@ import { Ion } from "cesium";
 
 Ion.defaultAccessToken = (import.meta as any).env.STORYBOOK_CESIUM_ACCESS_TOKEN ?? (import.meta as any).env.NEXT_PUBLIC_CESIUM_ACCESS_TOKEN
 
+console.log("process", process.env)
+console.log("import.meta", import.meta)
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
