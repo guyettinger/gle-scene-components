464ed2e4bf1736cf4f3cafa7af3af0ac56676f39
guyettinger <guy@flaire.co>
2024-08-30 14:30:35 -0400

    v1.0.29

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,7 @@
 import type { Preview } from "@storybook/react";
+import { Ion } from "cesium";
+
+Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjOWIzZmM0Mi0zOThjLTRmZjItOTM2OC1iOWM4N2ZiMzY3MzUiLCJpZCI6MTYzOTg4LCJpYXQiOjE2OTM2MDMzMzl9.SkbJ2XbsBVeaGSZ2MrwuksCHx4xuux3DGYh6pIJJhGQ"
 
 const preview: Preview = {
   parameters: {
